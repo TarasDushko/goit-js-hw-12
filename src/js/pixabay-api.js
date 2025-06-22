4fafa07c60e863e6b615189733397987efa61ca6
Taras <tarasdushko@gmail.com>
2025-06-22 12:38:33 +0200

    hw

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,23 +1,25 @@
 import axios from 'axios';
 
-const myKey = '50747549-8bc74331c649f9a4139cf6e6e';
+const myKey = '50629766-ca4e4f15305d377d5e3228fef';
 const url = 'https://pixabay.com/api/';
 
-export function getImagesByQuery(query) {
+export async function getImagesByQuery(query, page = 1, perPage = 15) {
   const params = {
     key: myKey,
     q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
+    page,
+    per_page: perPage,
   };
 
-  return axios.get(url, { params }).then(response => {
-    if (response.data.hits.length === 0) {
-      throw new Error(
-        'Sorry, there are no images matching your search query. Please try again!'
-      );
-    }
+  try {
+    const response = await axios.get(url, { params });
     return response.data;
-  });
+  } catch (error) {
+    throw new Error(
+      'Sorry, there are no images matching your search query. Please try again!'
+    );
+  }
 }
